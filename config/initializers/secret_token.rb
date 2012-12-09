6b45da142570a06268891180df519595282b4801
nailalves <nailalves@gmail.com>
2012-12-09 20:49:52 -0200

    Criando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stampgraff::Application.config.secret_token = '6eaa55566cf79e731bec3d2d540096e266be1202a1bc500b40358802212dad63fe960f3659afe6b69092635d407efd98679b67b3205794380f94ba90c7de90a9'
