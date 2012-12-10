5138402d09cf67fa6b86658539d0e5495ea85b2c
nailalves <nailalves@gmail.com>
2012-12-10 21:02:23 -0200

    Adicionando migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Stampgraff::Application.routes.draw do
+  root :to => "home#index"
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
