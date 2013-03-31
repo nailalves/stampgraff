3ea7dbf1bed35ede363ab2ff50d94288224386db
raonibr <raonidonordeste@gmail.com>
2013-03-30 22:08:25 -0300

    Coloquei o total num lugar mais razoavel

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,5 +2,5 @@ class Cliente < ActiveRecord::Base
 	has_many :ordems
   attr_accessible :celular, :email_empresa, :email_pessoal, :endereco, :name, :contato, :nascimento, :rg, :telefone, :celular2, :cpf, :cnpj, :razao_social, :status
 
-  validate_presence_of :celular, :name, :rg  
+  validates_presence_of :celular, :name, :rg  
 end
